db28174e6f3253f491e9b3ddf5542e26a22697e9
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-10 15:50:16 -0400

    square

--- main.py
+++ main.py
@@ -133,9 +133,9 @@ def init_viz():
 def update_viz(init_contents, contents, data):
 
 	# Construct labels
-	np_one = np.ones(contents.view(-1,3).shape[0]).astype(int)
+	np_one = np.ones(contents.view(-1,2).shape[0]).astype(int)
 	np_two = 2*np.ones(data.shape[0]).astype(int)
-	np_three = 3*np.ones(init_contents.view(-1,3).shape[0]).astype(int)
+	np_three = 3*np.ones(init_contents.view(-1,2).shape[0]).astype(int)
 
 	pts = np.row_stack((
 		contents.view(-1, 3).numpy(), 
