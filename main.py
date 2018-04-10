4165ccc7f5e48030afc1f5d24eafc02b31c978ca
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-10 17:53:53 -0400

    square

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def init_viz():
 		env=ENV,
 		win='mesh',
 		opts=dict(
-			opacity=0.1))
+			opacity=0.5))
 
 
 # contents is K x 3
@@ -176,10 +176,10 @@ def update_viz(init_contents, contents, data):
 		Y=np.random.rand(255),
 		opts=dict(
 			markersize=10,
-			markercolor=np.array([255, 0, 0])
+			markercolor=np.array([[0, 0, 255]])
 		),
+		name='1',
 		update='append',
-		env=ENV,
 		win='mesh')
 
 
