2a6e6152b24ad2efe68ffa9272ba28de6040ab57
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-10 17:21:57 -0400

    square

--- main.py
+++ main.py
@@ -127,6 +127,23 @@ def init_viz():
 		env=ENV,
 		win='grid')
 
+    x = [0, 0, 1, 1, 0, 0, 1, 1]
+    y = [0, 1, 1, 0, 0, 1, 1, 0]
+    z = [0, 0, 0, 0, 1, 1, 1, 1]
+    X = np.c_[x, y, z]
+    i = [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2]
+    j = [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3]
+    k = [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6]
+    Y = np.c_[i, j, k]
+    VIS.mesh(
+    	X=X, 
+    	Y=Y,
+    	env=ENV,
+    	win='mesh'
+    	opts=dict(
+    		opacity=0.5)
+    	)
+
 
 # contents is K x 3
 # data is N x 3
