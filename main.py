377164c0c302edc581c6d773d3668629357f74cf
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-11 09:42:35 -0400

    debugging

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ class SOM(object):
 		update = (weights[:, min_idx].unsqueeze(2) * diff)#.sum(1)
 
 		print torch.norm(update.permute(1,0,2)[0].view(self.rows, self.cols, -1),2,2)
-		r,c self._ind2sub(min_idx)
+		r,c = self._ind2sub(min_idx)
 		print r[0], c[0]
 		# print update.view(self.rows, self.cols, -1)
 		exit()
