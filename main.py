1d0d52019c08b8c4d0b903d5d69e44a018e64378
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-11 01:02:44 -0400

    sphere

--- main.py
+++ main.py
@@ -88,6 +88,7 @@ class SOM(object):
 		update = (weights[:, min_idx].unsqueeze(2) * diff)#.sum(1)
 
 		print update.permute(1,0,2)[0].view(self.rows, self.cols, -1)
+		print min_idx
 		# print update.view(self.rows, self.cols, -1)
 		exit()
 
