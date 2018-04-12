7b4515be44c341587db1d829d1c821c79abfc2aa
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-12 16:33:04 -0400

    batchsom

--- som.py
+++ som.py
@@ -163,7 +163,7 @@ class BatchSOM(SOM):
 			* freq_data).sum(0)
 
 		update = update_num / update_denom
-		print update[freq_data = 0]
+		# print update[freq_data == 0]
 
 
 		# print (update_num / update_denom)[freq_data>0, :]
