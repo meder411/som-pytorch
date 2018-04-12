67a8e3be7616a4f69910c52224de2cf9170b5952
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-12 17:36:37 -0400

    batchsom

--- som.py
+++ som.py
@@ -171,7 +171,7 @@ class BatchSOM(SOM):
 		# print 'freq'
 		# print freq_data.view(self.rows, self.cols)
 		print 'freq * weights'
-		print freq_weights[min_idx,:].view(-1, self.rows, self.cols)
+		print freq_weights[min_idx,:]
 		print 'num'
 		print update_num
 		print 'denom'
