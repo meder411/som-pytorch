667ccfc7b68599764d48354d50ea4cb7713a2305
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-17 14:35:11 -0400

    finishing som

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ def batch_main():
 	init_contents = som.contents.clone()
 
 	start = time.time()
-	for i in xrange(10):
+	for i in xrange(100):
 		# Generate some test data
 		if SHAPE == 'circle':
 			data = generateCirclePerimeter(N)
