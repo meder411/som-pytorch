9d57e8b328b90363f5645b62c4de87508985fea0
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-17 11:55:41 -0400

    batch som

--- som.py
+++ som.py
@@ -177,6 +177,8 @@ class BatchSOM(SOM):
 		print 'weighted data'
 		print (freq_weights.unsqueeze(2) * avg_data).sum(1)
 		print freq_weights.sum(1)
+
+		print (freq_weights.unsqueeze(2) * avg_data).sum(1) / freq_weights.sum(1)
 		exit()
 		
 
