4176d3365573337136b25dd711b87a96c664678f
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-19 19:40:00 -0400

    parbatch som

--- som.py
+++ som.py
@@ -192,8 +192,8 @@ class ParallelBatchSOM(SOM):
 
 
 		# Compute the update
-		print freq_weights.unsqueeze(-1)
-		print avg_data.unsqueeze(1)
+		print freq_weights
+		print avg_data
 		print (freq_weights.unsqueeze(-1) * avg_data.unsqueeze(1))
 		update_num = (freq_weights.unsqueeze(-1) * avg_data).sum(1)
 		update_denom = freq_weights.sum(1)
