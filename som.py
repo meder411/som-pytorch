2029c3e23ab2525f8101306727464f0ef6536d3b
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-19 15:02:42 -0400

    parbatch som

--- som.py
+++ som.py
@@ -150,9 +150,9 @@ class ParallelBatchSOM(SOM):
 		SOM.initialize(self, shape)
 
 		# Replicate the initialize BatchSOM B times
-		self.contents.repeat(self.batches, 1, 1, 1)
-		self.grid.repeat(self.batches, 1, 1, 1)
-		self.grid_used.repeat(self.batches, 1, 1, 1)
+		self.contents = self.contents.repeat(self.batches, 1, 1, 1)
+		self.grid = self.grid.repeat(self.batches, 1, 1, 1)
+		self.grid_used = self.grid_used.repeat(self.batches, 1, 1, 1)
 
 		print 'self.contents.shape'
 		print self.contents.shape
