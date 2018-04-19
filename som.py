94323c2f91f2fb05d4d48c7680991047eea38f00
Marc Eder <6818607+meder411@users.noreply.github.com>
2018-04-19 11:40:16 -0400

    finishing som

--- som.py
+++ som.py
@@ -208,8 +208,9 @@ class BatchSOM(SOM):
 
 		# Find the index of the best matching unit
 		_, min_idx = dist.topk(k=k, dim=1, largest=False)
-
 		print min_idx
+		
+		print dist.min(1)
 
 		# Return indices
 		return min_idx
